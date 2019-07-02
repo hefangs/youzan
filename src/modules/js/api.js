971e08e967563fe111f193b3d1976a7b84f017d7
he529564582 <he529564582@163.com>
2019-07-02 21:10:11 +0800

    更新api

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -21,8 +21,8 @@ let url = {
 }
 
 //开发环境和真实环境的切换
- let host = 'http://rapapi.org/mockjsdata/23334'
-// let host = 'http://rapapi.org/mockjsdata/24170'
+// let host = 'http://rapapi.org/mockjsdata/23334'
+ let host = 'http://rapapi.org/mockjsdata/24170'
 
 for (let key in url) {
   if (url.hasOwnProperty(key)) {
