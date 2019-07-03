38552f0ed705ace2a7c9bc1c390f848b964050bb
he529564582 <he529564582@163.com>
2019-07-03 21:53:19 +0800

    add topList subList rank searchList

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -4,25 +4,13 @@ let url = {
   topList: '/category/topList',
   subList: '/category/subList',
   rank: '/category/rank',
-  searchList: '/search/list',
-  details: '/goods/details',
-  deal: '/goods/deal',
-  cartAdd: '/cart/add',
-  cartRemove: '/cart/remove',
-  cartMremove: '/cart/mremove',
-  cartReduce: '/cart/reduce',
-  cartLists: '/cart/list',
-  cartUpdate: '/cart/update',
-  addressLists: '/address/list',
-  addressAdd: '/address/add',
-  addressRemove: '/address/remove',
-  addressUpdate: '/address/update',
-  addressSetDefault: '/address/setDefault'
+  searchList: '/search/list'
 }
 
 //开发环境和真实环境的切换
 // let host = 'http://rapapi.org/mockjsdata/23334'
- let host = 'http://rapapi.org/mockjsdata/24170'
+let host = 'http://rapapi.org/mockjsdata/24170'
+  // let host = 'http://rap2api.taobao.org/app/mock/7058'
 
 for (let key in url) {
   if (url.hasOwnProperty(key)) {
