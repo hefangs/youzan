3ecb8680ce70a25a1e7b5514c94b9eb4458d8082
he529564582 <he529564582@163.com>
2019-07-04 22:05:29 +0800

    add details.api

--- src/modules/js/api.js
+++ src/modules/js/api.js
@@ -4,13 +4,15 @@ let url = {
   topList: '/category/topList',
   subList: '/category/subList',
   rank: '/category/rank',
-  searchList: '/search/list'
+  searchList: '/search/list',
+  details: '/goods/details',
+  deal: '/goods/deal',
+  cartAdd: '/cart/add'
 }
 
 //开发环境和真实环境的切换
 // let host = 'http://rapapi.org/mockjsdata/23334'
 let host = 'http://rapapi.org/mockjsdata/24170'
-  // let host = 'http://rap2api.taobao.org/app/mock/7058'
 
 for (let key in url) {
   if (url.hasOwnProperty(key)) {
