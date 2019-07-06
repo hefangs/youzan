dd881b300d1fddbb99dfbcd462822c79c646a0d1
he529564582 <he529564582@163.com>
2019-07-06 17:36:17 +0800

    update build

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,16 +1,17 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var vueLoaderConfig = require('./vue-loader.conf')
+'use strict'
+const path = require('path')
+const utils = require('./utils')
+const config = require('../config')
+const vueLoaderConfig = require('./vue-loader.conf')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
 
+
+
 module.exports = {
-  // entry: {
-  //   app: './src/main.js'
-  // },
+  context: path.resolve(__dirname, '../'),
   entry: utils.entries(),
   output: {
     path: config.build.assetsRoot,
@@ -26,6 +27,7 @@ module.exports = {
       '@': resolve('src'),
       'css': '@/modules/css',
       'js': '@/modules/js',
+      'json': '@/modules/json',
       'components': '@/components'
     }
   },
@@ -39,7 +41,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
+        include: [resolve('src'), resolve('test'), resolve('node_modules/webpack-dev-server/client')]
       },
       {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
@@ -66,5 +68,17 @@ module.exports = {
         }
       }
     ]
+  },
+  node: {
+    // prevent webpack from injecting useless setImmediate polyfill because Vue
+    // source contains it (although only uses it if it's native).
+    setImmediate: false,
+    // prevent webpack from injecting mocks to Node native modules
+    // that does not make sense for the client
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
   }
 }
