5fa5780d6cb1a245de3dddd6f2496f05a873a785
he529564582 <he529564582@163.com>
2019-07-06 17:51:37 +0800

    add 首页

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,57 +1,68 @@
 import 'css/common.css'
 import './index.css'
 
-import Vue from 'vue'
-import axios from 'axios'
 import url from 'js/api.js'
-import Foot from 'components/Foot.vue'
-import Swipe from 'components/Swipe.vue'
+import axios from 'axios'
+import Vue from 'vue'
 
-import { InfiniteScroll } from 'mint-ui'
-Vue.use(InfiniteScroll)
+import Swiper from 'components/Swiper.vue'
+import mixin from 'js/mixin.js'
 
+import { InfiniteScroll } from 'mint-ui';
+
+Vue.use(InfiniteScroll);
+
+Vue.config.productionTip = false;
+
+
+/* eslint-disable no-new */
 new Vue({
-  el: '.vue-el',
+  el: '#app',
   data: {
+    lists: null,
     pageNum: 1,
     pageSize: 6,
-    lists: null,
     loading: false,
     allLoaded: false,
-    bannerLists: null
+    bannerLists: null,
+    pageLoading: true
   },
-  created() {
-    this.getLists()
-    this.getBanner()
+  created(){
+    Promise.all([this.getLists(),this.getBannerLists()]).then(res=>{
+      this.pageLoading = false;
+    })
   },
   methods: {
-    getLists() {
-      if (this.allLoaded) return
-      this.loading = true
-      axios.post(url.hotLists, {
-        pageNum: this.pageNum,
-        pageSize: this.pageSize
-      }).then(res => {
-        let curLists = res.data.lists
-        if (curLists.length < this.pageSize) {
-          this.allLoaded = true
-        }
-        if (this.lists) {
-          this.lists = this.lists.concat(curLists)
-        } else {
-          this.lists = curLists
-        }
-        this.loading = false
-      })
+    getLists(){
+        if(this.allLoaded) return;
+
+        this.loading = true;
+        return axios.get(url.hotList,{
+          pageNum: this.pageNum,
+          pageSize: this.pageSize
+        }).then(res => {
+          let currentLists = res.data.lists;
+
+          this.allLoaded =  currentLists.length < this.pageSize;
+
+          if(this.pageNum === 1){
+            this.lists = currentLists;
+          }else {
+            this.lists = this.lists.concat(currentLists);
+          }
+          this.pageNum++;
+          this.loading = false;
+        })
     },
-    getBanner() {
-      axios.get(url.banner).then(res => {
-        this.bannerLists = res.data.lists
+    getBannerLists(){
+      return axios.get(url.bannerList).then(res=>{
+        this.bannerLists = res.data.lists;
       })
     }
   },
   components: {
-    Foot,
-    Swipe
-  }
+    Swiper
+  },
+  mixins: [mixin]
+
 })
